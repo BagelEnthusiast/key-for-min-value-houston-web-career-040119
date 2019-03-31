8bdb76c77c66c38a4d8922ca74f1bf37173d1193
Nathan Richardson <nathan.richardson94@gmail.com>
2019-03-31 15:44:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(name_hash)
   name_of_min = nil 
   name_hash.each do |name, number|
     
-    if min_num.nil? || number < min_numum
-      minNum = number
+    if min_num.nil? || number < min_num
+      min_num = number
       name_of_min = name 
     end
   
